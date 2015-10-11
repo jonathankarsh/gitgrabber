09a286eb7e5be7910b500998b4ca61678033b8cf
Jonathan Karsh <jonathan.karsh@nearmap.com>
2015-10-11 21:53:56 +1100

    gitgrabber is a proof of concept libgit2sharp capabilities

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibGit2Sharp;
+
+namespace gitgrabber
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var path = Repository.Clone("https://github.com/jonathankarsh/latlong", "./latlong");
+            var repo = new Repository(path);
+            foreach (var commit in repo.Commits) {
+                Console.WriteLine("commit: {0} {1}", commit.Sha, commit.MessageShort);
+            }
+        }
+    }
+}
